5e15c1d5faafed8bb6d22a69ce30d7934cecb7e6
Zane <zane.franklin@gtri.gatech.edu>
2017-07-19 13:48:03 -0400

    setup script update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ if [ $PWD != terminal_setup ]; then
     exit 1
 fi
 
+ln -s $PWD $HOME/.terminal_setup
 ln -s $PWD $HOME/.vim
+ln -s $PWD/tmux-plugins $HOME/.tmux
 
 for f in $PWD/dotfiles/*; do 
     echo "$f -> $HOME/.$(basename $f)"
