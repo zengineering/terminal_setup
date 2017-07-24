619325f8c59005ff25cb6464a7471f04feb5f488
Zane <zane.franklin@gtri.gatech.edu>
2017-07-24 15:10:54 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-if [ $PWD != terminal_setup ]; then
+if [ $(basename $PWD) != terminal_setup ]; then
     echo "Run from terminal_setup dir."
     exit 1
 fi
@@ -9,5 +9,8 @@ ln -s $PWD/tmux-plugins $HOME/.tmux
 
 for f in $PWD/dotfiles/*; do 
     echo "$f -> $HOME/.$(basename $f)"
+    if [ -f $HOME/.$(basename $f) ]; then
+        mv $HOME/.$(basename $f) $HOME/.$(basename $f).bak
+    fi
     ln -s $f $HOME/.$(basename $f)
 done
