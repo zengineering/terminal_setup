aa928c6c5b62ba0dfa752ecfe6ab24f73cfa7652
Zane <zane.franklin@gtri.gatech.edu>
2017-10-23 10:42:29 -0400

    zsh

---
+++ install_zsh.sh
@@ -0,0 +1,26 @@
+function install_zsh() {
+    cd zsh
+    ./configure --prefix=$PREFIX
+    make -j8
+    make install
+    make install.info
+}
+
+# run in zsh
+function setup_prezto() {
+    etopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+}
+
+
+
+if [ $(basename $PWD) != terminal_setup ]; then
+    echo "Run from terminal_setup dir."
+    exit 1
+fi
+
+PREFIX=$HOME/.local
+install_zsh
+
