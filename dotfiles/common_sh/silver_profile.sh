fb19f66e6b919b3bc66ef2d6b8724ceee48f4421
zane <njineer@outlook.com>
2018-02-04 21:49:14 -0500

    fix typo in silver profile

--- dotfiles/common_sh/silver_profile.sh
+++ dotfiles/common_sh/silver_profile.sh
@@ -3,5 +3,5 @@ export JAVA_HOME="$HOME/.local/jdk-9.0.4"
 export KOTLIN_HOME="$HOME/.local/kotlinc"
 export LUA_HOME="$HOME/.local/lua"
 
-export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLINE_HOME/bin:$LUA_HOME/bin:$PATH"
+export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLIN_HOME/bin:$LUA_HOME/bin:$PATH"
 
