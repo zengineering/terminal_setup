cf4b6e7b8ebb91decf0397a39d05e304b3e58dac
zane <njineer@outlook.com>
2018-02-04 21:36:24 -0500

    update silver profile

--- dotfiles/common_sh/silver_profile.sh
+++ dotfiles/common_sh/silver_profile.sh
@@ -1,6 +1,7 @@
 export GOROOT="$HOME/.local/go"
 export JAVA_HOME="$HOME/.local/jdk-9.0.4"
 export KOTLIN_HOME="$HOME/.local/kotlinc"
+export LUA_HOME="$HOME/.local/lua"
 
-export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLINE_HOME/bin:$PATH"
+export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLINE_HOME/bin:$LUA_HOME/bin:$PATH"
 
