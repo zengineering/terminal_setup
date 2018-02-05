29f92dd2126d26deb53677df57ac797c33d3a4a2
zane <njineer@outlook.com>
2018-02-04 21:27:27 -0500

    add machine-specific profile

---
+++ dotfiles/common_sh/silver_profile.sh
@@ -0,0 +1,6 @@
+export GOROOT="$HOME/.local/go"
+export JAVA_HOME="$HOME/.local/jdk-9.0.4"
+export KOTLIN_HOME="$HOME/.local/kotlinc"
+
+export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLINE_HOME/bin:$PATH"
+
