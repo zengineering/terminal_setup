829158fc8aa171e4219fb9a311c3baa0923fcf2f
zane <njineer@outlook.com>
2018-03-19 21:55:01 -0400

    add ruby to silver profile

--- dotfiles/common_sh/silver_profile.sh
+++ dotfiles/common_sh/silver_profile.sh
@@ -4,5 +4,6 @@ export JAVA_HOME="$HOME/.local/jdk1.8.0_161"
 export KOTLIN_HOME="$HOME/.local/kotlinc"
 export LUA_HOME="$HOME/.local/lua"
 
-export PATH="$HOME/.local/bin:$GOROOT/bin:$JAVA_HOME/bin:$KOTLIN_HOME/bin:$LUA_HOME/bin:$PATH"
+export PATH="$GOROOT/bin:$JAVA_HOME/bin:$KOTLIN_HOME/bin:$LUA_HOME/bin:$HOME/.rbenv/bin:$PATH"
 
+export RBENV_VERSION="2.5.0"
