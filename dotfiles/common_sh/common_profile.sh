602e04ca29a16dae692185376765b2a77e67e3a8
Zane <zane.franklin@gtri.gatech.edu>
2018-03-23 14:19:18 -0400

    add brew cleanup to brewuu for macos

--- dotfiles/common_sh/common_profile.sh
+++ dotfiles/common_sh/common_profile.sh
@@ -36,7 +36,7 @@ alias uncommit='git reset --mixed HEAD~'
 case $OSTYPE in darwin*)
     alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
     alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-    alias brewuu='brew update && brew upgrade'
+    alias brewuu='brew update && brew upgrade && brew cleanup'
 ;; 
 esac
 
